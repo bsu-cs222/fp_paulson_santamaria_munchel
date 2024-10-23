d085ee01fb7c37e5842f653c4b1ccd88c583d10d
Jesus Santamaria <jesus.santamaria@bsu.edu>
2024-10-23 00:03:53 -0400

    Refactor GoogleMapsParser to contain more location information
    The new information includes the location's formatted address and the
    number of user ratings

--- lib/google_maps_parser.dart
+++ lib/google_maps_parser.dart
@@ -1,12 +1,24 @@
 class GoogleMapsParser {
   GoogleMapsLocation parse(dynamic jsonData) {
-    GoogleMapsLocation location = GoogleMapsLocation(
-        displayName: jsonData['places'][0]['displayName']['text']);
+    final String displayName = jsonData['places'][0]['displayName']['text'];
+    final String formattedAddress = jsonData["places"][0]["formattedAddress"];
+    final int userRatingCount = jsonData["places"][0]["userRatingCount"];
+
+    final GoogleMapsLocation location = GoogleMapsLocation(
+        displayName: displayName,
+        formattedAddress: formattedAddress,
+        userRatingCount: userRatingCount);
     return location;
   }
 }
 
 class GoogleMapsLocation {
   final String displayName;
-  GoogleMapsLocation({required this.displayName});
+  final String formattedAddress;
+  final int userRatingCount;
+
+  GoogleMapsLocation(
+      {required this.displayName,
+      required this.formattedAddress,
+      required this.userRatingCount});
 }
--- test/google_maps_parser_test.dart
+++ test/google_maps_parser_test.dart
@@ -7,11 +7,28 @@ import 'package:test/test.dart';
 void main() {
   GoogleMapsParser parser = GoogleMapsParser();
 
-  test('Able to get Roots Burger Bar from json data', () async {
+  test('Able to get Roots Burger Bar display name from json data', () async {
     final jsonObject =
         await _loadSampleData("test/ResturantsNearBallState.json");
     final location = parser.parse(jsonObject);
     expect(location.displayName, "Roots Burger Bar");
+  });
+
+  test('Able to get Roots Burger Bar formatted address from json data',
+      () async {
+    final jsonObject =
+        await _loadSampleData("test/ResturantsNearBallState.json");
+    final location = parser.parse(jsonObject);
+    expect(location.formattedAddress,
+        "1700 W University Ave, Muncie, IN 47303, USA");
+  });
+
+  test('Able to get Roots Burger Bar user rating count from json data',
+      () async {
+    final jsonObject =
+        await _loadSampleData("test/ResturantsNearBallState.json");
+    final location = parser.parse(jsonObject);
+    expect(location.userRatingCount, 878);
   });
 }
 
