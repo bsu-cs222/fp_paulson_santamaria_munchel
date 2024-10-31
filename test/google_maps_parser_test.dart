20bbb8dac55bb0495e6d4a35019360caf7828118
Jesus Santamaria <jesus.santamaria@bsu.edu>
2024-10-31 13:16:35 -0400

    Refactor google_maps_parser_test to have one test

--- test/google_maps_parser_test.dart
+++ test/google_maps_parser_test.dart
@@ -7,28 +7,14 @@ import 'package:test/test.dart';
 void main() {
   GoogleMapsParser parser = GoogleMapsParser();
 
-  test('Able to get David Owsley Museum display name from json data', () async {
+  test('Parser parses David Owsley Museum information correctly', () async {
     final jsonObject =
         await _loadSampleData("test/LocationsNearBallState.json");
     final locationList = parser.parse(jsonObject);
     expect(locationList[0].displayName,
         "David Owsley Museum of Art at Ball State University");
-  });
-
-  test('Able to get David Owsley Museum formatted address from json data',
-      () async {
-    final jsonObject =
-        await _loadSampleData("test/LocationsNearBallState.json");
-    final locationList = parser.parse(jsonObject);
     expect(locationList[0].formattedAddress,
         "Ball State University, Fine Arts Building, 2021 W Riverside Ave, Muncie, IN 47306, United States");
-  });
-
-  test('Able to get David Owsley Museum user rating count from json data',
-      () async {
-    final jsonObject =
-        await _loadSampleData("test/LocationsNearBallState.json");
-    final locationList = parser.parse(jsonObject);
     expect(locationList[0].userRatingCount, 218);
   });
 }
